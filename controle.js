a7fe35cbc3c774ae87c445eb27b73669c9f840b7
rodayna-web <nan460887@gmail.com>
2026-05-03 14:28:11 +0100

    Update controle.js

--- controle.js
+++ controle.js
@@ -1,19 +1,11 @@
 document.getElementById("form").addEventListener("submit", function(e) {
     e.preventDefault();
-    let nom = document.getElementById("nom").value;
+
     let symptomes = document.getElementById("symptomes").value.toLowerCase();
 
-    let resultat = "";
-
-    if (symptomes.includes("fièvre") || symptomes.includes("toux")) {
-        resultat = " grippe";
-    } 
-    else if (symptomes.includes("fatigue")) {
-        resultat = "fatigue légère";
-    } 
-    else {
-        resultat = "🩺 consulte un médecin";
+    if (symptomes.includes("fièvre")) {
+        document.getElementById("resultat").innerText = "grippe 🤒";
+    } else {
+        document.getElementById("resultat").innerText = "consulte médecin 🩺";
     }
-
-    document.getElementById("resultat").innerText = nom + " → " + resultat;
 });
