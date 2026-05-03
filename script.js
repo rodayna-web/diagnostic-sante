281e579b1af89c041955150ad80bc8a27fc2273a
rodayna-web <nan460887@gmail.com>
2026-05-03 14:07:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 document.getElementById("form").addEventListener("submit", function(e) {
     e.preventDefault();
+
     let nom = document.getElementById("nom").value;
-    let email = document.getElementById("email").value;
     let symptomes = document.getElementById("symptomes").value.toLowerCase();
 
     let resultat = "";
 
-    if (symptomes.includes("fièvre") || symptomes.includes("toux")) {
-        resultat = "Peut-être une grippe 🤒";
-    } 
-    else if (symptomes.includes("fatigue")) {
-        resultat = "Vous êtes fatigué 😴";
-    } 
-    else {
+    if (symptomes.includes("fièvre")) {
+        resultat = "Peut-être grippe 🤒";
+    } else {
         resultat = "Consultez un médecin 🩺";
     }
 
-    document.getElementById("resultat").innerText =
-        nom + " → " + resultat;
+    document.getElementById("resultat").innerText = nom + " → " + resultat;
 });
