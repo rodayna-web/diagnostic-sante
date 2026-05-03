caf525ea925e69e8046ed0dbc094c49f28b59571
rodayna-web <nan460887@gmail.com>
2026-05-03 14:05:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.getElementById("form").addEventListener("submit", function(e) {
     e.preventDefault();
+    let nom = document.getElementById("nom").value;
+    let email = document.getElementById("email").value;
     let symptomes = document.getElementById("symptomes").value.toLowerCase();
 
     let resultat = "";
 
-    if (symptomes.includes("fièvre")) {
-        resultat = "Peut-être grippe 🤒";
+    if (symptomes.includes("fièvre") || symptomes.includes("toux")) {
+        resultat = "Peut-être une grippe 🤒";
     } 
-    else if (symptomes.includes("toux")) {
-        resultat = "Possible infection légère 🤧";
+    else if (symptomes.includes("fatigue")) {
+        resultat = "Vous êtes fatigué 😴";
     } 
     else {
         resultat = "Consultez un médecin 🩺";
     }
 
-    document.getElementById("resultat").innerText = resultat;
+    document.getElementById("resultat").innerText =
+        nom + " → " + resultat;
 });
